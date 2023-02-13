8a0b6001da09d0f4c6425324e83c210609229788
“Joe <morel06joe@gmail.com>
2023-02-13 14:51:49 -0400

    Adding Modals, New Post fuctions, Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
+import MainHeader from "./components/MainHeader";
 import PostsList from "./components/PostsList";
 
 function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
+  function showModalHandler() {
+    setModalIsVisible(true);
+  }
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+
   return (
-    <main>
-      <PostsList />
-    </main>
+    <>
+      <MainHeader onCreatePost={showModalHandler} />
+      <main>
+        <PostsList
+          isPosting={modalIsVisible}
+          onStopPosting={hideModalHandler}
+        />
+      </main>
+    </>
   );
 }
 
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
+import Modal from "./Modal";
+import NewPost from "./NewPost";
 import Post from "./Post";
 
-import classes from  './PostsList.module.css';
+import classes from "./PostsList.module.css";
 
-function PostsList() {
+function PostsList({isPosting, onStopPosting}) {
+
+
+
   return (
-    <ul className={classes.posts}>
-      <Post author="Joe" body="React.js is awesome!" />
-      <Post author="Laura" body="Por fin Joelito!" />
-    </ul>
+    <>
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost
+        // onBodyChange={bodyChangeHandler}
+        //  onAuthorChange={authorChangeHandler} 
+         onCancel={onStopPosting}         
+          />
+       </Modal>
+      )}
+      <ul className={classes.posts}>
+  
+        <Post author="Laura" body="Por fin Joelito!" />
+      </ul>
+    </>
   );
 }
 
